87756287a1a3682a939c10b4fab7a949ee93be9e
Jonathan Morris <jonathan_wellington@icloud.com>
2024-04-24 11:50:02 -0700

    Cooking

--- lane_following_sac.py
+++ lane_following_sac.py
@@ -13,11 +13,29 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 
-import SAC.buffer as buffer
-import SAC.networks as networks
-import SAC.sac_torch as sac_torch
-
 import custom_gym
+from stable_baselines3 import SAC
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 env = custom_gym.CustomEnv(device, port=2000)
+
+model = SAC("MlpPolicy", env, verbose=1)
+
+model.learn(total_timesteps=10000)
+
+model.save("sac_carla")
+
+del model
+
+model = SAC.load("sac_carla")
+
+env.reset()
+
+while True:
+    action, _states = model.predict(obs, deterministic=True)
+    obs, reward, terminated, truncated, info = env.step(action)
+
+    print(reward)
+
+    if terminated or truncated:
+        obs, info = env.reset()
