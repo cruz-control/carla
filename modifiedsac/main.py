eb3b69d96f817609185b289bdef4f378659a6afc
Jonathan Morris <jonathan_wellington@icloud.com>
2024-05-12 21:10:06 -0700

    Added code

---
+++ modifiedsac/main.py
@@ -0,0 +1,117 @@
+from sac_torch import Agent
+import carla
+import random
+import numpy as np
+import cv2
+
+HEIGHT = 84
+WIDTH = 84
+CHANNELS = 3
+
+sensor_data = {"image": None}
+
+
+agent = Agent(input_dims=(HEIGHT, WIDTH, CHANNELS), n_actions=1)
+
+
+def process_img(image, sensor_data):
+    i = np.array(image.raw_data)
+    i2 = i.reshape((HEIGHT, WIDTH, 4))
+    i3 = i2[:, :, :3]
+    sensor_data["image"] = i3
+
+
+actor_list = []
+
+try:
+    client = carla.Client("localhost", 2000)
+    client.set_timeout(10.0)
+
+    print("Loading")
+
+    world = client.get_world()
+
+    blueprint_library = world.get_blueprint_library()
+
+    bp = blueprint_library.filter("model3")[0]
+    print(bp)
+
+    spawn_point = random.choice(world.get_map().get_spawn_points())
+
+    vehicle = world.spawn_actor(bp, spawn_point)
+    vehicle.set_autopilot(True)  # if you just wanted some NPCs to drive.
+
+    actor_list.append(vehicle)
+
+    # https://carla.readthedocs.io/en/latest/cameras_and_sensors
+    # get the blueprint for this sensor
+    blueprint = blueprint_library.find("sensor.camera.rgb")
+    # change the dimensions of the image
+    blueprint.set_attribute("image_size_x", f"{WIDTH}")
+    blueprint.set_attribute("image_size_y", f"{HEIGHT}")
+    blueprint.set_attribute("fov", "110")
+
+    # Adjust sensor relative to vehicle
+    spawn_point = carla.Transform(carla.Location(x=2.5, z=0.7))
+
+    # spawn the sensor and attach to vehicle.
+    sensor = world.spawn_actor(blueprint, spawn_point, attach_to=vehicle)
+
+    # add sensor to list of actors
+    actor_list.append(sensor)
+
+    # do something with this sensor
+    sensor.listen(lambda data: process_img(data, sensor_data))
+
+    while True:
+        world.tick()
+        if sensor_data["image"] is not None:
+            cv2.imshow("rgb", sensor_data["image"])
+            cv2.waitKey(1)
+
+            image = sensor_data["image"]
+            observation = image
+
+            action = agent.choose_action(observation)
+
+            steering = action[0]
+            throttle = 0.5
+            brake = 0.0
+
+            control = carla.VehicleControl(throttle, steering, brake)
+            vehicle.apply_control(control)
+
+            world.tick()
+
+            observation_ = image
+
+            waypoint = world.get_map().get_waypoint(
+                vehicle.get_location(),
+                project_to_road=True,
+                lane_type=(
+                    carla.LaneType.Driving
+                    | carla.LaneType.Shoulder
+                    | carla.LaneType.Sidewalk
+                ),
+            )
+
+            transform = waypoint.transform
+            loc = transform.location
+            vehicle_loc = vehicle.get_transform().location
+
+            l2_dist = np.sqrt(
+                (loc.x - vehicle_loc.x) ** 2 + (loc.y - vehicle_loc.y) ** 2
+            )
+
+            reward = -l2_dist
+            done = False
+
+            agent.remember(observation, action, reward, observation_, done)
+            agent.learn()
+
+
+finally:
+    print("destroying actors")
+    for actor in actor_list:
+        actor.destroy()
+    print("done.")
--- modifiedsac/networks.py
+++ modifiedsac/networks.py
@@ -6,9 +6,20 @@ import torch.optim as optim
 from torch.distributions.normal import Normal
 import numpy as np
 
+# Input dimms = 256 * 6 * 6, 4096
+
+
 class CriticNetwork(nn.Module):
-    def __init__(self, beta, input_dims, n_actions, fc1_dims=256, fc2_dims=256,
-            name='critic', chkpt_dir='tmp/sac'):
+    def __init__(
+        self,
+        beta,
+        input_dims,
+        n_actions,
+        fc1_dims=256,
+        fc2_dims=256,
+        name="critic",
+        chkpt_dir="tmp/sac",
+    ):
         super(CriticNetwork, self).__init__()
         self.input_dims = input_dims
         self.fc1_dims = fc1_dims
@@ -16,18 +27,39 @@ class CriticNetwork(nn.Module):
         self.n_actions = n_actions
         self.name = name
         self.checkpoint_dir = chkpt_dir
-        self.checkpoint_file = os.path.join(self.checkpoint_dir, name+'_sac')
+        self.checkpoint_file = os.path.join(self.checkpoint_dir, name + "_sac")
 
-        self.fc1 = nn.Linear(self.input_dims[0]+n_actions, self.fc1_dims)
+        self.alex_net = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(64, 192, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+        self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
+
+        self.fc1 = nn.Linear((input_dims[0] + n_actions), self.fc1_dims)
         self.fc2 = nn.Linear(self.fc1_dims, self.fc2_dims)
         self.q = nn.Linear(self.fc2_dims, 1)
 
         self.optimizer = optim.Adam(self.parameters(), lr=beta)
-        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
+        self.device = T.device("cuda:0" if T.cuda.is_available() else "cpu")
 
         self.to(self.device)
 
     def forward(self, state, action):
+        state = self.alex_net(state)
+        state = self.avgpool(state)
+        state = T.flatten(state, 1)
+
         action_value = self.fc1(T.cat([state, action], dim=1))
         action_value = F.relu(action_value)
         action_value = self.fc2(action_value)
@@ -43,27 +75,55 @@ class CriticNetwork(nn.Module):
     def load_checkpoint(self):
         self.load_state_dict(T.load(self.checkpoint_file))
 
+
 class ValueNetwork(nn.Module):
-    def __init__(self, beta, input_dims, fc1_dims=256, fc2_dims=256,
-            name='value', chkpt_dir='tmp/sac'):
+    def __init__(
+        self,
+        beta,
+        input_dims,
+        fc1_dims=256,
+        fc2_dims=256,
+        name="value",
+        chkpt_dir="tmp/sac",
+    ):
         super(ValueNetwork, self).__init__()
         self.input_dims = input_dims
         self.fc1_dims = fc1_dims
         self.fc2_dims = fc2_dims
         self.name = name
         self.checkpoint_dir = chkpt_dir
-        self.checkpoint_file = os.path.join(self.checkpoint_dir, name+'_sac')
+        self.checkpoint_file = os.path.join(self.checkpoint_dir, name + "_sac")
+
+        self.alex_net = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(64, 192, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+        self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
 
         self.fc1 = nn.Linear(*self.input_dims, self.fc1_dims)
         self.fc2 = nn.Linear(self.fc1_dims, fc2_dims)
         self.v = nn.Linear(self.fc2_dims, 1)
 
         self.optimizer = optim.Adam(self.parameters(), lr=beta)
-        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
+        self.device = T.device("cuda:0" if T.cuda.is_available() else "cpu")
 
         self.to(self.device)
 
     def forward(self, state):
+        state = self.alex_net(state)
+        state = self.avgpool(state)
+
         state_value = self.fc1(state)
         state_value = F.relu(state_value)
         state_value = self.fc2(state_value)
@@ -79,9 +139,19 @@ class ValueNetwork(nn.Module):
     def load_checkpoint(self):
         self.load_state_dict(T.load(self.checkpoint_file))
 
+
 class ActorNetwork(nn.Module):
-    def __init__(self, alpha, input_dims, max_action, fc1_dims=256, 
-            fc2_dims=256, n_actions=2, name='actor', chkpt_dir='tmp/sac'):
+    def __init__(
+        self,
+        alpha,
+        input_dims,
+        max_action,
+        fc1_dims=256,
+        fc2_dims=256,
+        n_actions=2,
+        name="actor",
+        chkpt_dir="tmp/sac",
+    ):
         super(ActorNetwork, self).__init__()
         self.input_dims = input_dims
         self.fc1_dims = fc1_dims
@@ -89,9 +159,26 @@ class ActorNetwork(nn.Module):
         self.n_actions = n_actions
         self.name = name
         self.checkpoint_dir = chkpt_dir
-        self.checkpoint_file = os.path.join(self.checkpoint_dir, name+'_sac')
+        self.checkpoint_file = os.path.join(self.checkpoint_dir, name + "_sac")
         self.max_action = max_action
         self.reparam_noise = 1e-6
+
+        self.alex_net = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(64, 192, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+        self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
 
         self.fc1 = nn.Linear(*self.input_dims, self.fc1_dims)
         self.fc2 = nn.Linear(self.fc1_dims, self.fc2_dims)
@@ -99,11 +186,14 @@ class ActorNetwork(nn.Module):
         self.sigma = nn.Linear(self.fc2_dims, self.n_actions)
 
         self.optimizer = optim.Adam(self.parameters(), lr=alpha)
-        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
+        self.device = T.device("cuda:0" if T.cuda.is_available() else "cpu")
 
         self.to(self.device)
 
     def forward(self, state):
+        state = self.alex_net(state)
+        state = self.avgpool(state)
+
         prob = self.fc1(state)
         prob = F.relu(prob)
         prob = self.fc2(prob)
@@ -125,9 +215,9 @@ class ActorNetwork(nn.Module):
         else:
             actions = probabilities.sample()
 
-        action = T.tanh(actions)*T.tensor(self.max_action).to(self.device)
+        action = T.tanh(actions) * T.tensor(self.max_action).to(self.device)
         log_probs = probabilities.log_prob(actions)
-        log_probs -= T.log(1-action.pow(2)+self.reparam_noise)
+        log_probs -= T.log(1 - action.pow(2) + self.reparam_noise)
         log_probs = log_probs.sum(1, keepdim=True)
 
         return action, log_probs
